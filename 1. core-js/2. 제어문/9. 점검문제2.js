5f3befd917409412a7528b8571dc8b9c2534de86
conan <sarimiro@naver.com>
2023-07-26 09:42:20 +0900

    updown게임 두 번째 버전- 카운트다운 추가

--- 1. core-js/2. 제어문/9. 점검문제2.js
+++ 1. core-js/2. 제어문/9. 점검문제2.js
@@ -4,17 +4,24 @@
 // 만약 정답을 맞추지 못하면 지속해서 입력을 받고 결과를 알려준다.
 // 정답을 맞추면 맞췄다는 메시지와 함께 프로그램을 종료한다.
 
-// 사용자들은 입력 기회가 5번으로 제한된다. 
+// 사용자들은 입력 기회가 5번으로 제한된다.
 // 즉, 5번 안에 정답을 맞추지 못할 경우 게임이 강제종료된다.
 
-// 사용자는 게임 시작전에 난이도를 설정할 수 있다. 
-// 난이도는 상, 중, 하 난이도가 존재하며 
+// 사용자는 게임 시작전에 난이도를 설정할 수 있다.
+// 난이도는 상, 중, 하 난이도가 존재하며
 // 난이도별 입력 횟수제한이 다르다.
 
 // 실제로 정답으로 사용할 랜덤 정수 만들기
 let secret = Math.floor(Math.random() * 100) + 1;
+
 // 입력 최소, 최대값
 let min = 1, max = 100;
+
+// 초기 카운트 수
+let initCount = 5;
+
+// 카운트다운 변수
+let countdown = initCount;
 
 while (true) {
 
@@ -28,10 +35,12 @@ while (true) {
     continue;
   }
 
+  countdown--; // 카운트 감소
+
   // 업 다운 판단
   // 정답인 경우
   if (secret === answer) {
-    alert(`정답입니다!`);
+    alert(`정답입니다! ${initCount - countdown}번만에 맞췄습니다!`); // 게임종료 조건 1
     break;
   } else if (secret > answer) {
     alert(`UP!!`);
@@ -40,8 +49,19 @@ while (true) {
     alert(`DOWN!!`);
     max = answer - 1;
   }
-}
+
+  // 추가 게임 종료 조건
+  if (countdown === 0) {
+    alert(`ㅠㅠ 패배하셨습니다! 정답은 ${secret}였지롱 ㅋㅋ`);
+    break;
+  } else {
+    alert(`${countdown}번의 기회가 남았습니다.`);
+  }
+
+} // end while loop
+
 alert(`수고하셨습니다!`);
+
 
 // let lv = prompt('[ 1. 상 (기회 3번) | 2. 중 (기회 6번) | 3. 하 (기회 10번) ]');
 
