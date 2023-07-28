933e13598e084875fc7389da1ffab31dcde1210e
conan <sarimiro@naver.com>
2023-07-28 10:44:21 +0900

    core js - 스코프

---
+++ 1. core-js/6. 함수/10. let과 const.js
@@ -0,0 +1,32 @@
+
+let x;
+console.log(x);
+
+// const y;
+// y = 100;
+// console.log(y);
+
+const COLOR_YELLOW = '#dd0';
+const COLOR_GREEN = '#0f0';
+const COLOR_BLACK = '#000';
+
+$div.style.background = COLOR_YELLOW;
+$span.style.background = COLOR_YELLOW;
+$div.style.background = COLOR_GREEN;
+$div.style.background = COLOR_BLACK;
+
+let kim = {
+    mame: '김철수',
+    age: 30
+};
+kim.name = '김마이클';
+// kim = {
+//     mame: '박영희',
+//     age: 40
+// };
+console.log(kim);
+
+let a = 10;
+const b = 20;   // 잘 모르겠을 땐 const로 지정해두다가 바꿀 일이 생기면 let으로 변경
+
+// for (let i = 0; i < 10; i++) {}  // for문은 const 사용 금지.
