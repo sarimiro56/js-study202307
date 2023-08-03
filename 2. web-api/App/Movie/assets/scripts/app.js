7e7499f7a259bafe27f8fd51caec5220c05fd6c0
conan <sarimiro@naver.com>
2023-08-03 11:37:03 +0900

    web api - 실전예제 2: 영화 삭제 기능

--- 2. web-api/App/Movie/assets/scripts/app.js
+++ 2. web-api/App/Movie/assets/scripts/app.js
@@ -12,7 +12,7 @@ const $confirmAddMovieButton = $cancelAddMovieButton.nextElementSibling;
 
 // 영화 추가 모달 안에 있는 입력엘리먼트들
 const $userInputs = [
-  ...$addMovieModal.querySelectorAll("input"),
+  ...$addMovieModal.querySelectorAll("input")
 ];
 const [$titleInput, $imgUrlInput, $ratingInput] = $userInputs;
 const $entryTextSection = document.getElementById('entry-text');
@@ -25,61 +25,115 @@ const CLASS_VISIBLE = 'visible';
 // 영화 정보 목록 배열
 const movies = [];
 
-// ===== 유틸 함수, 일반 함수 정의 ===== //
+// ===== 유틸함수, 일반함수 정의 ===== //
 
 // 모든 인풋을 리셋하는 함수
 const clearMovieModalInput = () => {
-    $userInputs.forEach($input => $input.value = '');
+  $userInputs.forEach($input => $input.value = '');
 };
 
-// ===== 유틸 함수, 일반 함수 정의 ===== //
-
-// 영화 추가 모달을 닫는 함수
+// 영화추가모달을 닫는 함수
 const closeAddModal = () => {
-    $backdrop.classList.remove(CLASS_VISIBLE);
-    $addMovieModal.classList.remove(CLASS_VISIBLE);
-    clearMovieModalInput();
+  $backdrop.classList.remove(CLASS_VISIBLE);
+  $addMovieModal.classList.remove(CLASS_VISIBLE);
+  clearMovieModalInput();
 };
 
 // 화면에 새로운 영화 정보를 렌더링하는 함수
-const renderNewMovie = ({title, image, rating}) => {
-    const $newMovie = document.createElement('li');
-    $newMovie.classList.add('movie-element');
+const renderNewMovie = ({ id, title, image, rating }) => {
+  const $newMovie = document.createElement('li');
+  $newMovie.classList.add('movie-element');
+  $newMovie.dataset.movieId = id;
 
-    $newMovie.innerHTML = `
+  $newMovie.innerHTML = `
     <div class="movie-element__image">
-        <img src="${image}" alt="${title}">
+      <img src="${image}" alt="${title}">
     </div>
     <div class="movie-element__info">
-        <h2>${title}</h2>
-        <p>${rating} / 5</p>
+      <h2>${title}</h2>
+      <p>${rating} / 5</p>
     </div>
-    `;
+  `;
 
-    $movieList.appendChild($newMovie);
+  // 삭제를 진행하는 핸들러
+  const deleteMovieHandler = e => {
+
+    // 배열에서도 영화 정보를 지워야 함!
+    // 클릭한 태그의 근처 li의 movie-id값 가져오기
+    const movieId = e.target.closest('.movie-element').dataset.movieId;
+    console.log(movieId);
+
+    // 배열에서 해당 아이디값을 가지는 객체를 찾아내고 인덱스를 알아내기
+    // let index = -1;
+    // for (let i = 0; i < movies.length; i++) {
+    //   if (movies[i].id === movieId) {
+    //     index = i;
+    //     break;
+    //   }
+    // }
+
+    // 대상의 인덱스 찾기
+    // indexOf: 원시타입 (숫자, 문자열)만 찾을 수 있음
+    // findIndex: 배열 고차함수 
+    const index = movies.findIndex(m => m.id === movieId);
+    console.log(`클릭대상 인덱스: ${index}`);
+
+    // 그 객체를 배열에서 지우기 <- ?? : 인덱스를 알아야 지우지
+    movies.splice(index, 1);
+    
+    // 실제 li 지우기
+    e.target.closest('.movie-element').remove();
+  };
+
+  // 삭제 클릭 이벤트
+  $newMovie.addEventListener('click', deleteMovieHandler)
+
+  $movieList.appendChild($newMovie);
 };
+
+// 영화 정보 입력란 검증 
+const validateMovieInput = ({title, image, rating}) => {
+  if (
+    title.trim() === '' || 
+    image.trim() === '' || 
+    rating.trim() === '' ||
+    +rating < 1 || +rating > 5
+  ) {
+    return false;
+  }
+  return true;
+}
+
 // =====  이벤트 핸들러 및 이벤트 바인딩 ===== //
 
 // 영화 추가버튼 기능을 수행하는 핸들러
 const addMovieHandler = e => {
-    const titleValue = $titleInput.value; //제목 입력란 값
-    const imgUriValue = $imgUrlInput.value; //이미지경로
-    const ratingValue = $ratingInput.value; //평점 입력값
+  const titleValue = $titleInput.value; // 제목입력란값
+  const imgUrlValue = $imgUrlInput.value; // 이미지경로
+  const ratingValue = $ratingInput.value; // 평점입력값
 
-    // 객체로 묶기
-    const newMovie = {
-        title: titleValue,
-        image: imgUriValue,
-        rating: ratingValue
-    };
-    // console.log(newMovie);
-    movies.push(newMovie);
-    console.log(movies);
+  // 객체로 묶기
+  const newMovie = {
+    id: Math.random().toString(),
+    title: titleValue,
+    image: imgUrlValue,
+    rating: ratingValue
+  };
 
-    // 모달 닫기
-    closeAddModal();
-    // 화면에 입력한 영화정보 렌더링하기
-    renderNewMovie(newMovie);
+  // 검증
+  if (!validateMovieInput(newMovie)) {
+    alert('입력값이 유효하지 않습니다!');
+    return;
+  }
+
+  // console.log(newMovie);
+  movies.push(newMovie);
+  console.log(movies);
+
+  // 모달 닫기
+  closeAddModal();
+  // 화면에 입력한 영화정보 렌더링하기
+  renderNewMovie(newMovie);
 };
 
 // 영화 추가 모달창을 띄우는 핸들러
@@ -88,14 +142,15 @@ const showMovieModalHandler = e => {
   $backdrop.classList.add(CLASS_VISIBLE);
 };
 
+
+
 // 백드롭 영역을 클릭하면 모달이 닫히는 핸들러
 const backdropHandler = e => {
-    closeAddModal();
+  closeAddModal();
 };
-// 영화 추가 모달창의 취소 버튼을 누르면 모달이 닫히는 핸들러
+// 영화 추가 모달창의 취소버튼을 누르면 모달이 닫히는 핸들러
 const closeMovieModalHandler = e => {
-    $backdrop.classList.remove(CLASS_VISIBLE);
-    $addMovieModal.classList.remove(CLASS_VISIBLE);
+  closeAddModal();
 };
 
 // Add movie버튼 클릭이벤트
@@ -104,7 +159,7 @@ $addMovieButton.addEventListener('click', showMovieModalHandler);
 // backdrop영역 클릭이벤트
 $backdrop.addEventListener('click', backdropHandler);
 
-// Add Movie 모달 취소버튼 클릭 이벤트
+// Add Movie모달 취소버튼 클릭이벤트
 $cancelAddMovieButton.addEventListener('click', closeMovieModalHandler);
 
 // Add Movie모달 추가버튼 클릭이벤트
